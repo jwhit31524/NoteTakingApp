64c8cf6769a98c7a67199031a89dc163c06f897b
J Whitton <jwhitton316@gmail.com>
2023-12-30 14:20:11 -0500

    Writes text to a text file and displays it upon loading app

--- NoteTaker/Form1.Designer.cs
+++ NoteTaker/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.EnterTextLabel = new System.Windows.Forms.Label();
+            this.NoteTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(184, 280);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // EnterTextLabel
+            // 
+            this.EnterTextLabel.AutoSize = true;
+            this.EnterTextLabel.Location = new System.Drawing.Point(193, 53);
+            this.EnterTextLabel.Name = "EnterTextLabel";
+            this.EnterTextLabel.Size = new System.Drawing.Size(114, 13);
+            this.EnterTextLabel.TabIndex = 1;
+            this.EnterTextLabel.Text = "Enter Note Text Below";
+            this.EnterTextLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NoteTextBox
+            // 
+            this.NoteTextBox.Location = new System.Drawing.Point(184, 69);
+            this.NoteTextBox.Multiline = true;
+            this.NoteTextBox.Name = "NoteTextBox";
+            this.NoteTextBox.Size = new System.Drawing.Size(250, 205);
+            this.NoteTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NoteTextBox);
+            this.Controls.Add(this.EnterTextLabel);
+            this.Controls.Add(this.submitButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label EnterTextLabel;
+        private System.Windows.Forms.TextBox NoteTextBox;
     }
 }
 
--- NoteTaker/Form1.cs
+++ NoteTaker/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
 
 namespace NoteTaker
 {
@@ -15,6 +17,36 @@ namespace NoteTaker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {/*
+         (StreamReader inputFile = new StreamReader(Path.Combine("C:/temp1", "Note.txt")))
+            {
+
+                NoteTextBox.Text = inputFile.ReadLine();
+            }*/
+
+        
+                NoteTextBox.Text = File.ReadAllText("C:/temp1/Note.txt");
+          
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine("C:/temp1", "Note.txt")))
+            {
+               
+                    outputFile.WriteLine(NoteTextBox.Text);
+            }
+
         }
     }
 }
